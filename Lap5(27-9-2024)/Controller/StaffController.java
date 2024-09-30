44d4519c010cf37609f26103b43ab92555713ace
Hieuaptech <167809500+Hieuaptechw@users.noreply.github.com>
2024-09-30 10:43:49 +0700

    admin
    admin

--- Lap5(27-9-2024)/Controller/StaffController.java
+++ Lap5(27-9-2024)/Controller/StaffController.java
@@ -24,7 +24,7 @@ public class StaffController {
         return
                 staffs.stream()
                         .filter(s->s.getId().toLowerCase().contains(sfaffid.toLowerCase()))
-                        .collect(Collectors.toList());
+                        .findFirst();
 
     }
     public Optional<Staff> maxPayFeMale() {
